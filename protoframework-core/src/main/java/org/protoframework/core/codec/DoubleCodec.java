c78fc08e20cef2f950a35f96c7b92b5eac38627d
yuan <yuan@yzyw-inc.com>
2018-07-05 12:27:05 +0800

    ADD: codec for float and double

---
+++ protoframework-core/src/main/java/org/protoframework/core/codec/DoubleCodec.java
@@ -0,0 +1,33 @@
+package org.protoframework.core.codec;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author: yuanwq
+ * @date: 2018/7/5
+ */
+public class DoubleCodec implements ICodec<Double> {
+  public static final DoubleCodec INSTANCE = new DoubleCodec();
+
+  private DoubleCodec() {
+  }
+
+  private ICodec<String> nativeCodec() {
+    return StringCodec.INSTANCE;
+  }
+
+  @Override
+  public Double decode(@Nullable byte[] data) {
+    if (data == null) return null;
+    return Double.valueOf(nativeCodec().decode(data));
+  }
+
+  @Override
+  public byte[] encode(@Nullable Double v) {
+    if (v == null) {
+      return null;
+    }
+    return nativeCodec().encode(String.valueOf(v));
+  }
+
+}
---
+++ protoframework-core/src/main/java/org/protoframework/core/codec/FloatCodec.java
@@ -0,0 +1,33 @@
+package org.protoframework.core.codec;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author: yuanwq
+ * @date: 2018/7/5
+ */
+public class FloatCodec implements ICodec<Float> {
+  public static final FloatCodec INSTANCE = new FloatCodec();
+
+  private FloatCodec() {
+  }
+
+  private ICodec<String> nativeCodec() {
+    return StringCodec.INSTANCE;
+  }
+
+  @Override
+  public Float decode(@Nullable byte[] data) {
+    if (data == null) return null;
+    return Float.valueOf(nativeCodec().decode(data));
+  }
+
+  @Override
+  public byte[] encode(@Nullable Float v) {
+    if (v == null) {
+      return null;
+    }
+    return nativeCodec().encode(String.valueOf(v));
+  }
+
+}
