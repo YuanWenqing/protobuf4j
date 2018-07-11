1c057dd711c625bcf693883a598c4cac7ded1a46
yuan <yuan@yzyw-inc.com>
2018-07-11 22:12:47 +0800

    ADD: sql表达式相关接口

---
+++ protoframework-data/src/main/java/org/protoframework/sql/IBinaryExpr.java
@@ -0,0 +1,34 @@
+package org.protoframework.sql;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+/**
+ * author: yuanwq
+ * date: 2018/7/11
+ *
+ * @param <T></T> 操作符类型
+ */
+public interface IBinaryExpr<T extends ISqlOperation<? extends IBinaryExpr<T>>>
+    extends IExpression {
+  /**
+   * 左表达式
+   */
+  IExpression getLeft();
+
+  /**
+   * 右表达式
+   */
+  IExpression getRight();
+
+  /**
+   * 操作符
+   */
+  T getOp();
+
+  @Override
+  default int comparePrecedence(@Nonnull ISqlOperation outerOp) {
+    // 默认：操作符不同，就需要包裹括号
+    return Objects.equals(getOp(), outerOp) ? 0 : -1;
+  }
+}
---
+++ protoframework-data/src/main/java/org/protoframework/sql/ISqlOperation.java
@@ -0,0 +1,62 @@
+package org.protoframework.sql;
+
+import javax.annotation.Nonnull;
+
+/**
+ * 访问者模式解耦具体表达式的sql语句构造逻辑
+ * <p>
+ * author: yuanwq
+ * date: 2018/7/11
+ *
+ * @param <T> 处理的表达式类型
+ */
+public interface ISqlOperation<T extends IBinaryExpr<? extends ISqlOperation<T>>> {
+  String WRAP_LEFT = "(";
+  String WRAP_RIGHT = ")";
+
+  default void toSqlTemplate(@Nonnull T expr, @Nonnull StringBuilder sb) {
+    boolean needWrap = expr.getLeft().comparePrecedence(this) < 0;
+    if (needWrap) {
+      sb.append(WRAP_LEFT);
+    }
+    expr.getLeft().toSqlTemplate(sb);
+    if (needWrap) {
+      sb.append(WRAP_RIGHT);
+    }
+    sb.append(this.getOp());
+    needWrap = expr.getRight().comparePrecedence(this) < 0;
+    if (needWrap) {
+      sb.append(WRAP_LEFT);
+    }
+    expr.getRight().toSqlTemplate(sb);
+    if (needWrap) {
+      sb.append(WRAP_RIGHT);
+    }
+  }
+
+  default void toSolidSql(@Nonnull T expr, @Nonnull StringBuilder sb) {
+    boolean needWrap = expr.getLeft().comparePrecedence(this) < 0;
+    if (needWrap) {
+      sb.append(WRAP_LEFT);
+    }
+    expr.getLeft().toSolidSql(sb);
+    if (needWrap) {
+      sb.append(WRAP_RIGHT);
+    }
+    sb.append(this.getOp());
+    needWrap = expr.getRight().comparePrecedence(this) < 0;
+    if (needWrap) {
+      sb.append(WRAP_LEFT);
+    }
+    expr.getRight().toSolidSql(sb);
+    if (needWrap) {
+      sb.append(WRAP_RIGHT);
+    }
+  }
+
+  /**
+   * 构造sql语句中的操作符形式，两边包括必需的空格
+   */
+  String getOp();
+
+}
