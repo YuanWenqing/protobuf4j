616ff0cee6f2bbaa1cc67182dbe52e18a4c860db
yuan <yuan@yzyw-inc.com>
2018-07-11 23:28:48 +0800

    ADD: raw expr sql

---
+++ protoframework-data/src/main/java/org/protoframework/sql/RawExpr.java
@@ -0,0 +1,67 @@
+package org.protoframework.sql;
+
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * author: yuanwq
+ * date: 2018/7/11
+ */
+public class RawExpr implements IExpression {
+
+  private final String rawSqlExpr;
+  private final List<Object> values;
+
+  public RawExpr(@Nonnull String rawSqlExpr, @Nonnull Collection<?> values) {
+    this.rawSqlExpr = rawSqlExpr;
+    this.values = ImmutableList.copyOf(values);
+  }
+
+  public String getRawSqlExpr() {
+    return rawSqlExpr;
+  }
+
+  public List<Object> getValues() {
+    return values;
+  }
+
+  @Override
+  public int comparePrecedence(@Nonnull ISqlOperation outerOp) {
+    return 1;
+  }
+
+  @Override
+  public StringBuilder toSqlTemplate(@Nonnull StringBuilder sb) {
+    return sb.append(rawSqlExpr);
+  }
+
+  @Override
+  public StringBuilder toSolidSql(@Nonnull StringBuilder sb) {
+    Iterator<Object> iter = values.iterator();
+    for (String part : StringUtils.splitPreserveAllTokens(rawSqlExpr, "?")) {
+      sb.append(part);
+      if (iter.hasNext()) {
+        sb.append(iter.next());
+      } else {
+        sb.append("?");
+      }
+    }
+    return sb;
+  }
+
+  @Override
+  public List<Object> collectSqlValue(@Nonnull List<Object> collectedValues) {
+    collectedValues.addAll(values);
+    return collectedValues;
+  }
+
+  @Override
+  public String toString() {
+    return toSolidSql(new StringBuilder()).toString();
+  }
+}
