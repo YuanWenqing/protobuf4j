a548740b49a6c59513c3a3d94e6b03e777414bc6
yuan <yuan@yzyw-inc.com>
2018-07-12 22:37:19 +0800

    ADD: order by clause

---
+++ protoframework-data/src/main/java/org/protoframework/sql/Direction.java
@@ -0,0 +1,10 @@
+package org.protoframework.sql;
+
+/**
+ * author: yuanwq
+ * date: 2018/7/12
+ */
+public enum Direction {
+  ASC,
+  DESC
+}
---
+++ protoframework-data/src/main/java/org/protoframework/sql/clause/OrderByClause.java
@@ -0,0 +1,84 @@
+package org.protoframework.sql.clause;
+
+import com.google.common.collect.Lists;
+import org.protoframework.sql.IExpression;
+import org.protoframework.sql.ISqlStatement;
+import org.protoframework.sql.expr.TableColumn;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * author: yuanwq
+ * date: 2018/7/12
+ */
+public class OrderByClause implements ISqlStatement {
+  private final List<OrderByExpr> orderByExprs = Lists.newArrayList();
+
+  public List<OrderByExpr> getOrderByExprs() {
+    return Collections.unmodifiableList(orderByExprs);
+  }
+
+  public OrderByClause by(OrderByExpr orderByExpr) {
+    this.orderByExprs.add(orderByExpr);
+    return this;
+  }
+
+  public OrderByClause by(IExpression expr) {
+    return by(new OrderByExpr(expr));
+  }
+
+  public OrderByClause by(String column) {
+    return by(new TableColumn(column));
+  }
+
+  @Override
+  public StringBuilder toSqlTemplate(@Nonnull StringBuilder sb) {
+    if (orderByExprs.isEmpty()) {
+      return sb;
+    }
+    sb.append("ORDER BY ");
+    boolean first = true;
+    for (OrderByExpr expr : orderByExprs) {
+      if (first) {
+        first = false;
+      } else {
+        sb.append(",");
+      }
+      expr.toSqlTemplate(sb);
+    }
+    return sb;
+  }
+
+  @Override
+  public StringBuilder toSolidSql(@Nonnull StringBuilder sb) {
+    if (orderByExprs.isEmpty()) {
+      return sb;
+    }
+    sb.append("ORDER BY ");
+    boolean first = true;
+    for (OrderByExpr expr : orderByExprs) {
+      if (first) {
+        first = false;
+      } else {
+        sb.append(",");
+      }
+      expr.toSolidSql(sb);
+    }
+    return sb;
+  }
+
+  @Override
+  public List<Object> collectSqlValue(@Nonnull List<Object> collectedValues) {
+    for (OrderByExpr expr : orderByExprs) {
+      expr.collectSqlValue(collectedValues);
+    }
+    return collectedValues;
+  }
+
+  @Override
+  public String toString() {
+    return toSolidSql(new StringBuilder()).toString();
+  }
+}
