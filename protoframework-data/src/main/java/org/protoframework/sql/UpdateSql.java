8484788aa2385bc06fe6ba4b001f7129fb32bfd6
yuan <yuan@yzyw-inc.com>
2018-07-15 17:00:53 +0800

    ADD: update sql

---
+++ protoframework-data/src/main/java/org/protoframework/sql/UpdateSql.java
@@ -0,0 +1,82 @@
+package org.protoframework.sql;
+
+import com.google.common.base.Preconditions;
+import org.protoframework.sql.clause.SetClause;
+import org.protoframework.sql.clause.WhereClause;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * author: yuanwq
+ * date: 2018/7/15
+ */
+public class UpdateSql implements ISqlStatement {
+  private final ITableRef table;
+  private final SetClause set;
+  private WhereClause where;
+
+  public UpdateSql(@Nonnull ITableRef table, @Nonnull SetClause set) {
+    Preconditions.checkNotNull(table);
+    Preconditions.checkNotNull(set);
+    this.table = table;
+    this.set = set;
+  }
+
+  public UpdateSql(@Nonnull ITableRef table, @Nonnull SetClause set, WhereClause where) {
+    this(table, set);
+    this.setWhere(where);
+  }
+
+  public ITableRef getTable() {
+    return table;
+  }
+
+  public SetClause getSet() {
+    return set;
+  }
+
+  public WhereClause getWhere() {
+    return where;
+  }
+
+  public UpdateSql setWhere(WhereClause where) {
+    this.where = where;
+    return this;
+  }
+
+  @Override
+  public StringBuilder toSqlTemplate(@Nonnull StringBuilder sb) {
+    sb.append("UPDATE ");
+    table.toSqlTemplate(sb);
+    sb.append(" ");
+    set.toSqlTemplate(sb);
+    if (where != null) {
+      sb.append(" ");
+      where.toSqlTemplate(sb);
+    }
+    return sb;
+  }
+
+  @Override
+  public StringBuilder toSolidSql(@Nonnull StringBuilder sb) {
+    sb.append("UPDATE ");
+    table.toSolidSql(sb);
+    sb.append(" ");
+    set.toSolidSql(sb);
+    if (where != null) {
+      sb.append(" ");
+      where.toSolidSql(sb);
+    }
+    return sb;
+  }
+
+  @Override
+  public List<Object> collectSqlValue(@Nonnull List<Object> collectedValues) {
+    set.collectSqlValue(collectedValues);
+    if (where != null) {
+      where.collectSqlValue(collectedValues);
+    }
+    return collectedValues;
+  }
+}
