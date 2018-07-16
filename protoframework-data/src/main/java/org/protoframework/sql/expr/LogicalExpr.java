d9d6b5d6cd5c37115c14be6a32651d3c086c19af
yuan <yuan@yzyw-inc.com>
2018-07-16 18:13:09 +0800

    ADD: AbstractBinaryExpr

--- protoframework-data/src/main/java/org/protoframework/sql/expr/LogicalExpr.java
+++ protoframework-data/src/main/java/org/protoframework/sql/expr/LogicalExpr.java
@@ -1,10 +1,8 @@
 package org.protoframework.sql.expr;
 
-import org.protoframework.sql.IBinaryExpr;
 import org.protoframework.sql.IExpression;
 
 import javax.annotation.Nonnull;
-import java.util.List;
 
 /**
  * 逻辑表达式：AND OR XOR
@@ -13,49 +11,10 @@ import java.util.List;
  * @author: yuanwq
  * @date: 2018/7/11
  */
-public class LogicalExpr extends AbstractExpression implements IBinaryExpr<LogicalOp> {
-  private final IExpression left;
-  private final LogicalOp op;
-  private final IExpression right;
+public class LogicalExpr extends AbstractBinaryExpr<LogicalOp> {
 
   public LogicalExpr(IExpression left, @Nonnull LogicalOp op, IExpression right) {
-    this.left = left;
-    this.op = op;
-    this.right = right;
-  }
-
-  @Override
-  public IExpression getLeft() {
-    return left;
-  }
-
-  @Override
-  public IExpression getRight() {
-    return right;
-  }
-
-  @Override
-  public LogicalOp getOp() {
-    return op;
-  }
-
-  @Override
-  public StringBuilder toSqlTemplate(@Nonnull StringBuilder sb) {
-    this.op.toSqlTemplate(this, sb);
-    return sb;
-  }
-
-  @Override
-  public StringBuilder toSolidSql(@Nonnull StringBuilder sb) {
-    this.op.toSolidSql(this, sb);
-    return sb;
-  }
-
-  @Override
-  public List<Object> collectSqlValue(@Nonnull List<Object> collectedValues) {
-    this.left.collectSqlValue(collectedValues);
-    this.right.collectSqlValue(collectedValues);
-    return collectedValues;
+    super(left, op, right);
   }
 
 }
