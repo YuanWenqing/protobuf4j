e0bc8d5addea80eddce3d45e27cb514fecce9577
yuan <yuan@yzyw-inc.com>
2018-07-18 01:02:54 +0800

    ADD: testcase for SqlUtil

--- protoframework-data/src/test/java/org/protoframework/sql/TestSqlUtil.java
+++ protoframework-data/src/test/java/org/protoframework/sql/TestSqlUtil.java
@@ -1,6 +1,8 @@
 package org.protoframework.sql;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
+import org.protoframework.sql.expr.ArithmeticOp;
 
 import static org.junit.Assert.*;
 
@@ -16,5 +18,37 @@ public class TestSqlUtil {
     assertEquals("%a%", SqlUtil.likeSub("a"));
 
     assertEquals("%%a%", SqlUtil.likePrefix("%a"));
+  }
+
+  @Test
+  public void testStar() {
+    try {
+      System.out.println(SqlUtil.SELECT_STAR);
+      SqlUtil.SELECT_STAR.setAlias("a");
+      fail();
+    } catch (UnsupportedOperationException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testCount() {
+    try {
+      System.out.println(SqlUtil.SELECT_COUNT);
+      SqlUtil.SELECT_COUNT.setAlias("a");
+      fail();
+    } catch (UnsupportedOperationException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testAggregateWrap() {
+    IExpression expr = SqlUtil.aggregateWrap("sum", QueryCreator.column("a"));
+    System.out.println(expr);
+    assertEquals("sum(a)", expr.toSqlTemplate(new StringBuilder()).toString());
+    assertEquals("sum(a)", expr.toSolidSql(new StringBuilder()).toString());
+    assertTrue(expr.comparePrecedence(ArithmeticOp.ADD) > 0);
+    assertTrue(expr.collectSqlValue(Lists.newArrayList()).isEmpty());
   }
 }
