1d1816c642ff07c1b3cba51f8679680394b04d4e
yuan <yuan@yzyw-inc.com>
2018-07-17 19:37:52 +0800

    MOD: ArithmeticExpr使用静态方法构造

--- protoframework-data/src/main/java/org/protoframework/sql/expr/ArithmeticExpr.java
+++ protoframework-data/src/main/java/org/protoframework/sql/expr/ArithmeticExpr.java
@@ -12,8 +12,31 @@ import javax.annotation.Nonnull;
  * @date: 2018/7/11
  */
 public class ArithmeticExpr extends AbstractBinaryExpr<ArithmeticOp> {
-  public ArithmeticExpr(IExpression left, @Nonnull ArithmeticOp op, IExpression right) {
+  protected ArithmeticExpr(IExpression left, @Nonnull ArithmeticOp op, IExpression right) {
     super(left, op, right);
   }
 
+  public static ArithmeticExpr add(IExpression left, IExpression right) {
+    return new ArithmeticExpr(left, ArithmeticOp.ADD, right);
+  }
+
+  public static ArithmeticExpr sub(IExpression left, IExpression right) {
+    return new ArithmeticExpr(left, ArithmeticOp.SUBSTRACT, right);
+  }
+
+  public static ArithmeticExpr multi(IExpression left, IExpression right) {
+    return new ArithmeticExpr(left, ArithmeticOp.MULTIPLY, right);
+  }
+
+  public static ArithmeticExpr div(IExpression left, IExpression right) {
+    return new ArithmeticExpr(left, ArithmeticOp.DIVIDE, right);
+  }
+
+  public static ArithmeticExpr divRound(IExpression left, IExpression right) {
+    return new ArithmeticExpr(left, ArithmeticOp.DIV_ROUND, right);
+  }
+
+  public static ArithmeticExpr mod(IExpression left, IExpression right) {
+    return new ArithmeticExpr(left, ArithmeticOp.MOD, right);
+  }
 }
