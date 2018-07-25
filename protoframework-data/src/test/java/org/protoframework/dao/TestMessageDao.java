3b5272517063caeb2ffbec49b0f1f11fb9c6ee76
yuan <yuan@yzyw-inc.com>
2018-07-25 16:21:11 +0800

    MOD: test select

--- protoframework-data/src/test/java/org/protoframework/dao/TestMessageDao.java
+++ protoframework-data/src/test/java/org/protoframework/dao/TestMessageDao.java
@@ -1,14 +1,16 @@
 package org.protoframework.dao;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.protoframework.core.ProtoMessageHelper;
+import org.protoframework.core.proto.data.TestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.jdbc.JdbcTestUtils;
 
-import javax.sql.DataSource;
+import static org.junit.Assert.*;
 
 /**
  * @author: yuanwq
@@ -18,13 +20,30 @@ import javax.sql.DataSource;
 @ContextConfiguration(classes = JdbcConfiguration.class)
 public class TestMessageDao {
   @Autowired
-  private DataSource dataSource;
-  @Autowired
   private JdbcTemplate jdbcTemplate;
 
-  @Test
-  public void test() {
-    System.out.println(dataSource.getClass());
-    System.out.println(JdbcTestUtils.countRowsInTable(jdbcTemplate, "db_msg"));
+  private IPrimaryKeyMessageDao<Long, TestModel.DbMsg> dao;
+  private final String primaryKey = "id";
+
+  @Before
+  public void setup() {
+    dao = new PrimaryProtoMessageDao<>(TestModel.DbMsg.class, primaryKey);
+    ((PrimaryProtoMessageDao<Long, TestModel.DbMsg>) dao).setJdbcTemplate(jdbcTemplate);
   }
+
+  @Test
+  public void testDaoInfo() {
+    assertEquals(primaryKey, dao.getPrimaryKey());
+  }
+
+  @Test
+  public void testSelect() {
+    TestModel.DbMsg msg = dao.selectOneByPrimaryKey(1L);
+    System.out.println(ProtoMessageHelper.printToString(msg));
+    assertEquals(1L, msg.getId());
+    assertEquals(10, msg.getInt32V());
+    assertEquals(100L, msg.getInt64V());
+    assertTrue(0 < msg.getCreateTime() && msg.getCreateTime() <= System.currentTimeMillis());
+  }
+
 }
