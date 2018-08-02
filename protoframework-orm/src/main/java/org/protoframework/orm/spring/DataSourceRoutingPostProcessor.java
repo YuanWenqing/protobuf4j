334c8ed891d811f1ccea2f693cb01d7b52e5d43e
yuan <yuan@yzyw-inc.com>
2018-08-02 20:30:07 +0800

    MOD: check DataSourceRouting.value()

--- protoframework-orm/src/main/java/org/protoframework/orm/spring/DataSourceRoutingPostProcessor.java
+++ protoframework-orm/src/main/java/org/protoframework/orm/spring/DataSourceRoutingPostProcessor.java
@@ -1,11 +1,14 @@
 package org.protoframework.orm.spring;
 
+import org.apache.commons.lang3.StringUtils;
 import org.protoframework.orm.dao.IMessageDao;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import static com.google.common.base.Preconditions.*;
 
 /**
  * @author: yuanwq
@@ -24,6 +27,8 @@ public class DataSourceRoutingPostProcessor implements BeanPostProcessor {
     DataSourceRouting routing =
         AnnotationUtils.findAnnotation(bean.getClass(), DataSourceRouting.class);
     if (routing != null) {
+      checkArgument(StringUtils.isNotBlank(routing.value()),
+          "Blank value of DataSourceRouting on " + bean.getClass().getName());
       if (!(bean instanceof IMessageDao)) {
         throw new BeanNotOfRequiredTypeException(beanName, IMessageDao.class, bean.getClass());
       }
