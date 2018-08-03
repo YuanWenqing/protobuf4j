43a57ab0f4b47ea139435136cde25713878a50f4
yuan <yuan@yzyw-inc.com>
2018-08-03 15:57:22 +0800

    MOD: test aware

--- protoframework-orm/src/test/java/org/protoframework/orm/spring/TestDataSourceRouting.java
+++ protoframework-orm/src/test/java/org/protoframework/orm/spring/TestDataSourceRouting.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -48,4 +50,38 @@ public class TestDataSourceRouting {
     assertEquals(bDataSource, bDao.getJdbcTemplate().getDataSource());
     assertEquals(cDao.getJdbcTemplate(), bDao.getJdbcTemplate());
   }
+
+  @Autowired
+  private DSAware dsAware;
+  @Autowired
+  private JTAware jtAware;
+
+  @Test
+  public void testAware() {
+    assertEquals(aDataSource, dsAware.dataSource);
+    assertEquals(bDataSource, jtAware.jdbcTemplate.getDataSource());
+  }
+
+  @Component
+  @DataSourceRouting("a")
+  public static class DSAware implements DataSourceAware {
+    private DataSource dataSource;
+
+    @Override
+    public void setDataSource(DataSource dataSource) {
+      this.dataSource = dataSource;
+    }
+  }
+
+  @Component
+  @DataSourceRouting("b")
+  public static class JTAware implements JdbcTemplateAware {
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+      this.jdbcTemplate = jdbcTemplate;
+    }
+  }
+
 }
