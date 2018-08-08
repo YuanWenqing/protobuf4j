fbd7bc6d5ae24bdfa7e23330044491d843241338
yuan <yuan@yzyw-inc.com>
2018-08-08 11:42:40 +0800

    ADD: orm auto starter

---
+++ orm-starter/src/main/java/org/protoframework/orm/OrmAutoConfiguration.java
@@ -0,0 +1,24 @@
+package org.protoframework.orm;
+
+import org.protoframework.orm.spring.DataSourceRoutingPostProcessor;
+import org.protoframework.orm.spring.JdbcRoutingResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: yuanwq
+ * @date: 2018/8/8
+ */
+@Configuration
+public class OrmAutoConfiguration {
+
+  @Bean
+  public JdbcRoutingResolver jdbcRoutingResolver() {
+    return new JdbcRoutingResolver();
+  }
+
+  @Bean
+  public DataSourceRoutingPostProcessor dataSourceRoutingPostProcessor() {
+    return new DataSourceRoutingPostProcessor(jdbcRoutingResolver());
+  }
+}
