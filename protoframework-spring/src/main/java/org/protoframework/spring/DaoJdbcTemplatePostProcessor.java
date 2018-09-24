9306309f7d7c1297eb8364b45d14be291dee45c1
yuan <yuan@yzyw-inc.com>
2018-09-24 17:47:37 +0800

    MOD: 默认支持dao自动jdbc配置

---
+++ protoframework-spring/src/main/java/org/protoframework/spring/DaoJdbcTemplatePostProcessor.java
@@ -0,0 +1,36 @@
+package org.protoframework.spring;
+
+import org.protoframework.orm.dao.IMessageDao;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author: yuanwq
+ * @date: 2018/9/24
+ */
+class DaoJdbcTemplatePostProcessor implements BeanPostProcessor, ApplicationContextAware {
+  private ApplicationContext applicationContext;
+  private JdbcTemplate jdbcTemplate;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String beanName)
+      throws BeansException {
+    if (!(bean instanceof IMessageDao)) {
+      return bean;
+    }
+    IMessageDao<?> dao = (IMessageDao<?>) bean;
+    if (jdbcTemplate == null) {
+      jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+    }
+    dao.setJdbcTemplate(jdbcTemplate);
+    return dao;
+  }
+}
