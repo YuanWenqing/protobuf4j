addddc1edec25c3d6d6f46ad06de19bb478f29db
yuan <yuan@yzyw-inc.com>
2018-10-13 20:02:01 +0800

    MOD: lombok in sql

--- protoframework-sql/src/main/java/org/protoframework/orm/sql/expr/BetweenExpr.java
+++ protoframework-sql/src/main/java/org/protoframework/orm/sql/expr/BetweenExpr.java
@@ -1,5 +1,7 @@
 package org.protoframework.orm.sql.expr;
 
+import lombok.Data;
+import lombok.NonNull;
 import org.protoframework.orm.sql.IExpression;
 import org.protoframework.orm.sql.ISqlOperator;
 import org.protoframework.orm.sql.ISqlValue;
@@ -11,25 +13,15 @@ import java.util.List;
  * @author: yuanwq
  * @date: 2018/7/11
  */
+@Data
 public class BetweenExpr extends AbstractExpression {
   private static final String WRAP_LEFT = "(";
   private static final String WRAP_RIGHT = ")";
 
+  @NonNull
   private final IExpression min;
+  @NonNull
   private final IExpression max;
-
-  public BetweenExpr(@Nonnull IExpression min, @Nonnull IExpression max) {
-    this.min = min;
-    this.max = max;
-  }
-
-  public IExpression getMin() {
-    return min;
-  }
-
-  public IExpression getMax() {
-    return max;
-  }
 
   @Override
   public int comparePrecedence(@Nonnull ISqlOperator outerOp) {
