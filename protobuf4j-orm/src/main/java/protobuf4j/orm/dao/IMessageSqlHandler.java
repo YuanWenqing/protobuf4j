54c8f2846ddf9a94fa1abb029f901b9607308094
yuan <yuan@yzyw-inc.com>
2019-01-30 14:08:35 +0800

    MOD: sql handler

--- protobuf4j-orm/src/main/java/protobuf4j/orm/dao/IMessageSqlHandler.java
+++ protobuf4j-orm/src/main/java/protobuf4j/orm/dao/IMessageSqlHandler.java
@@ -6,26 +6,24 @@ package protobuf4j.orm.dao;
 /**
  * author yuanwq
  */
-public interface IMessageSqlHandler<T> {
+public interface IMessageSqlHandler {
 
   /**
-   * 将{@code beanClass}实例的字段值{@code value}转换为对应sql类型的值
+   * 将{@code field}的值{@code value}转换为对应sql类型的值
    *
-   * @param beanClass 处理转换的bean实例的类型
-   * @param field     {@code beanClass}的字段名
-   * @param value     {@code beanClass}实例的{@code field}字段的值
+   * @param field 字段名
+   * @param value 字段值
    * @return 转换后sql类型的值
    */
-  <B extends T> Object toSqlValue(Class<B> beanClass, String field, Object value);
+  Object toSqlValue(String field, Object value);
 
   /**
-   * 将sql类型的值{@code sqlValue}转换为{@code beanClass}中{@code field}字段的类型的值
+   * 将sql类型的值{@code sqlValue}转换为{@code field}的值
    *
-   * @param beanClass 处理转换的bean实例的类型
-   * @param field     {@code beanClass}的字段名
-   * @param sqlValue  sql类型的值
-   * @return 转换后{@code beanClass}中{@code field}字段的值
+   * @param field    字段名
+   * @param sqlValue sql类型的值
+   * @return 转换后{@code field}的字段值
    */
-  <B extends T> Object fromSqlValue(Class<B> beanClass, String field, Object sqlValue);
+  Object fromSqlValue(String field, Object sqlValue);
 
 }
